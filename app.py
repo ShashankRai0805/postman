e35fff6c5d0cc0a4d4805dea1346ae3c7479e260
Shashank Rai <shashankrai0805@gmail.com>
2024-09-01 16:29:54 +0530

    Added app.py and updated readme

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+stored_age = 26
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/sit')
+def sit():
+    return "SIT ROCKS!"
+
+@app.route('/sheela')
+def sheela():
+    return f"Sheela ki jawani! Umar {stored_age}"
+
+@app.route('/sheela/<age>')
+def sheela_age(age):
+    return f"Sheela ki umar {age}."
+
+@app.route('/sheela', methods=['POST'])
+def sheela_post():
+    global stored_age
+    stored_age = request.form.get('age')
+    return "Done!"
+
+app.run()
